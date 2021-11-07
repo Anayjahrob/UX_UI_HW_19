3e97d0e8ac80e81b6869ee26457d7d5178f05d39
Anayjah <anayjahrob19@gmail.com>
2021-11-06 18:08:18 -0700

    m

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+
+
+$(p).on("mouseenter":function(){
+      $(this).css("color", "teal");}
+    
+    "mouseleave": function(){
+      $(this).css("color", "white");
+    });
+    $(selector).animate({params},speed,callback);
